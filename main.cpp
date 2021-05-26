89d70fb642d8abc42a3110872e833ce4fac840ff
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-26 12:03:33 +0300

    Theory added

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include "counter/WordCounter.h"
 
 int main() {
-    Reader *reader = new FileReader();
-    std::vector<std::string> chars = reader->read("C:\\Users\\владислав\\CLionProjects\\OOP_mod_2\\testFile.txt");
-    WordCounter wordCounter;
-    std::map<std::string, int> m = wordCounter.getWordToFrequencyMap(chars);
-    auto it = m.begin();
-    while(it != m.end()){
-        std::cout<<it->first<<" :: "<<it->second<<std::endl;
-        it++;
-    }
+//    Reader *reader = new FileReader();
+//    std::vector<std::string> chars = reader->read("YourPath");
+//    WordCounter wordCounter;
+//    std::map<std::string, int> m = wordCounter.getWordToFrequencyMap(chars);
+//    auto it = m.begin();
+//    while(it != m.end()){
+//        std::cout<<it->first<<" :: "<<it->second<<std::endl;
+//        it++;
+//    }
     return 0;
 }
