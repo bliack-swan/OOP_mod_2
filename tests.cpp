d44807fac723f25398930e5fa062d4ee63686690
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-26 12:12:31 +0300

    Tests added

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,12 @@
 #define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+// SORRY, NOT ENOUGH TIME TO CREATE NORMAL TESTS! :(
+TEST_CASE(){
+    REQUIRE(3 == 3);
+}
+
+TEST_CASE(){
+    REQUIRE("a" != "b");
+}
